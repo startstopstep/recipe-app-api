9fffe90fccb9d6a3931c205d5cc96d9f337fe513
Yurii Merker <yura.merk11@gmail.com>
2023-02-04 16:03:43 +0200

    Write unit tests for test_db future features.

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,13 @@
+"""
+Django command for wait for the database to be available.
+"""
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Django command for wait for the database to the database.
+    """
+
+    def handle(self, *args, **options):
+        pass
