c59fa537b48a678358837239ebf46a0220f9ef0c
Yurii Merker <yura.merk11@gmail.com>
2023-02-06 20:13:30 +0200

    Add wait_for_db command and fix linter issues.

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,11 @@
 """
 Django command for wait for the database to be available.
 """
+import time
+
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg2Error
 
 
 class Command(BaseCommand):
@@ -10,4 +14,15 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **options):
-        pass
+        """ Entrypoint for command. """
+        self.stdout.write('Waiting for a database...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2Error, OperationalError):
+                self.stdout.write('Database unavailable. Waiting 1 second...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
