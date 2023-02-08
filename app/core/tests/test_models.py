9da606bda9961636f53d8184c83421ab27e6049a
Yurii Merker <yura.merk11@gmail.com>
2023-02-08 12:50:51 +0200

    Add test for User model, User model and User manager.

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
