923442bb79b1446bd59965fe8705fddbadf5eb75
Yurii Merker <yura.merk11@gmail.com>
2023-02-13 21:23:40 +0200

    Add drf-spectacular to the project.

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf-spectacular',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf-spectacular.openapi.AutoSchema',
+}
