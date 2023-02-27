04ea1aa4a3dfb6fc4cc72b1e309f12f047513c09
Yurii Merker <yuriim@proteantecs.com>
2023-02-27 13:43:43 +0200

    Add "Tag" model.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,13 @@
+from decimal import Decimal
+
+from core import models
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from decimal import Decimal
-from core import models
+
+
+def create_user(email='user@example.com', password='testpass123'):
+    """ Create and return a new user. """
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -54,3 +60,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """ Test creating a tag is successful. """
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
